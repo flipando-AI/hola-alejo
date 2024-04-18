0759aa2f7d9bf7bce5802721b46845c9b6b73457
yvesfogel <yvesfogel@gmail.com>
2024-04-18 15:08:13 -0300

    Add files via upload

---
+++ hola-alejo/main.py
@@ -0,0 +1,22 @@
+# Imports
+
+
+def get_total_characters():
+    return None
+
+
+def get_rick_episodes():
+    return None
+
+
+def main():
+    amount_chars = get_total_characters()
+    rick_episodes = get_rick_episodes()
+
+    print(f"There are {amount_chars} characters in the Rick and Morty API")
+    print(f"Rick appears in {rick_episodes} episodes")
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ hola-alejo/src/rick_and_morty.py
@@ -0,0 +1,9 @@
+import requests
+
+
+class RickAndMortyApi:
+    def __init__(self):
+        self.base_url = "https://rickandmortyapi.com/api"
+
+    def get_characters(self):
+        pass
